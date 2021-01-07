d394a408e241d020603187b596ae88ce4b6f767e
denis farkas <denis.farkas@laplateforme.io>
2021-01-07 14:20:59 +0100

    ajout data topics view home

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -2,14 +2,18 @@
 class Posts extends Controller {
     public function __construct() {
         $this->postModel = $this->model('Post');
+        $this->topicModel = $this->model('topic');
     }
 
     public function home()
     {
-        $data = [
-            'title' => 'Home page'
-        ];
+        $topics = $this->topicModel->findAllTopics();
 
+        $data = [
+            'title'=> 'Home page',
+            'topics' => $topics
+        ];
+      
         $this->view('posts/home', $data);
     }
 
--- app/views/posts/home.php
+++ app/views/posts/home.php
@@ -34,9 +34,18 @@
                     </div>
                         
                     <div class="col-lg-4 col-md-4">
-                    <?php
-require URLROOT . 'Topics/listTopics';
-?>  
+                    <div class="sidebarblock">
+        <h3>Topics</h3>
+        <div class="divline"></div>
+            <div class="blocktxt">
+                <ul class="cats">
+                    <?php foreach($data['topics'] as $topics){
+                        echo '<li><a href="#">'.$topics->titre.'<span class="badge pull-right">20</span></a></li>';
+                    } ?>
+                                                              
+                </ul>
+            </div>
+    </div>
                     </div>
 
         </section>
