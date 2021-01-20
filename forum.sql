45709ecfe8233d23dc4e127ee21b1d7f9ef0f5b5
Wiedenhofer <71873322+thucnhi-wiedenhofer@users.noreply.github.com>
2021-01-20 09:41:13 +0100

    Update forum.sql

--- forum.sql
+++ forum.sql
@@ -19,10 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `thuc-nhi-wiedenhofer_forum`
+-- Base de données :  `forum`
 --
-CREATE DATABASE IF NOT EXISTS `thuc-nhi-wiedenhofer_forum` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `thuc-nhi-wiedenhofer_forum`;
+CREATE DATABASE IF NOT EXISTS `forum` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `forum`;
 
 -- --------------------------------------------------------
 
